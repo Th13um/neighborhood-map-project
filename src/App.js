be807f7a29d4f0ba76f18a75edee3cd0ee9fa91b
Th13um <dthieum@gmail.com>
2018-08-29 01:37:44 +0200

    changes the way the map is displayed. add CSS and search box.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,185 @@
 import React, { Component } from 'react';
- import './App.css';
-import Map from './components/Map.js';
+import DataLocations from './data/Locations.json'
+import Map from './components/Map'
 
- class App extends Component {
+
+class App extends Component {
+
+  state = {
+    locations: []
+  }
+
+  constructor(props) {
+    super(props);
+    this.loadMap = this.loadMap.bind(this);
+    this.searchLocation = this.searchLocation.bind(this);
+    this.selectLocation = this.selectLocation.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      locations: DataLocations
+    })
+
+    window.loadMap = this.loadMap;
+    initJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyCWxhujtEkLYQauF0fHdEbrvHT_U3ZDRSE&callback=loadMap')
+  }
+
+
+  //initialize map
+  loadMap() {
+
+    const self = this;
+    const mapView = document.getElementById('mapContainer');
+
+    mapView.style.height = window.innerHeight + "px";
+
+    const map = new window.google.maps.Map(mapView, {
+      center: { lat: 48.864716, lng: 2.349014 }
+    });
+
+    this.map = map;
+    this.FacebookInfoWindow = new window.google.maps.InfoWindow();
+
+    // Set the Bounds of the Map to the Locations
+    const bounds = new window.google.maps.LatLngBounds();
+
+    // Create Markers
+    this.state.locations.forEach((location) => {
+      let myMarker = self.createMarker(location);
+      bounds.extend(myMarker.position);
+      location.marker = myMarker;
+    });
+
+    this.map.fitBounds(bounds);
+  };
+
+
+  //marker constructor
+  createMarker(location) {
+
+    const self = this
+
+    const marker = new window.google.maps.Marker({
+      position: {
+        lat: location.location.lat,
+        lng: location.location.lng
+      },
+      map: this.map,
+      title: location.name,
+      animation: window.google.maps.Animation.BOUNCE
+    })
+
+    marker.addListener('click', function () {
+      self.populateInfoWindow(this, self.FacebookInfoWindow);
+    });
+
+    return marker
+  };
+
+
+  // Display Info Window with Data
+  populateInfoWindow(marker, infowindow) {
+
+    if (infowindow.marker !== marker) {
+
+      // Center on clicked Marker
+      this.map.panTo(new window.google.maps.LatLng(marker.position.lat(), marker.position.lng()))
+      infowindow.marker = marker;
+
+      // Displays message whilst Facebook Data is being retrieved
+      infowindow.setContent('<div>Info</div>')
+      infowindow.open(this.map, marker)
+
+      // Event Listener - on Closing Window
+      infowindow.addListener('closeclick', function () {
+        infowindow.marker = null
+      });
+
+    }
+
+  }
+
+  //Filter the list of locations
+  searchLocation(query) {
+
+    let self = this;
+
+    if (query === '') {
+
+      // If no location in search field, set Locations to all
+      this.setState((state) => {
+        return {
+          locations: DataLocations
+        }
+      })
+
+      DataLocations.filter(location => {
+        return location.marker.setMap(self.map)
+      })
+    }
+    else {
+
+      //Search DataLocations for the query
+      let SearchedLocations = DataLocations.filter(location => {
+
+        if (location.marker) {
+          if (location.title.toLowerCase().indexOf(query.toLowerCase().trim()) >= 0) {
+            location.marker.setMap(self.map);
+          } else {
+            location.marker.setMap(null);
+          }
+        }
+        return location.title.toLowerCase().indexOf(query.toLowerCase()) > -1
+      }
+    )
+
+      // Set Locations
+      this.setState((state) => {
+        return {
+          locations: SearchedLocations
+        }
+      })
+
+
+    }
+  }
+
+
+
+
+  //Info for choosing location
+  selectLocation(data) {
+    this.populateInfoWindow(data.marker, this.FacebookInfoWindow)
+  }
+
+
 
   render() {
+
     return (
+
       <div className="App">
-         <Map />
-       </div>
-    );
+
+        <div id="mapContainer"></div>
+
+        <Map
+          locations={this.state.locations}
+          selectLocation={this.selectLocation}
+          searchLocation={this.searchLocation} />
+
+      </div>
+    )
   }
 }
- export default App;
+
+export default App;
+
+
+function initJS(src) {
+  const ref = window.document.getElementsByTagName("script")[0];
+  const script = window.document.createElement("script");
+  script.src = src;
+  script.async = true;
+  ref.parentNode.insertBefore(script, ref);
+}
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,60 +1,35 @@
-import React, { Component } from 'react';
-import DataLocations from '../data/Locations.json';
-const { compose, withProps, withStateHandlers } = require("recompose");
-const { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } = require( 'react-google-maps');
-
- class Map extends Component {
-
-       render() {
-
-           const ParisGoogleMap = compose(
-               withStateHandlers(() => ({
-                   isOpen: false,
-               }), {
-                       onToggleOpen: ({ isOpen }) => () => ({
-                           isOpen: !isOpen,
-                       })
-                   }),
-               withScriptjs,
-               withGoogleMap
-           )(props =>
-               <GoogleMap
-                   defaultCenter={{ lat: 48.864716, lng: 2.349014 }}
-                   defaultZoom={12}
-               >
-
-                   {DataLocations.map((marker) =>
-
-                       <Marker
-                           name={marker.title}
-                           onClick={props.onToggleOpen}
-                           position={{ lat: marker.location.lat, lng: marker.location.lng }}
-                           id={marker.id}
-                       >
-                           {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
-                               <div> info </div>
-                           </InfoWindow>}
-
-                       </Marker>
-
-                   )}
-
-
-               </GoogleMap>
-           );
-
-           return (
-               <div>
-                   <ParisGoogleMap
-                       googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWxhujtEkLYQauF0fHdEbrvHT_U3ZDRSE&v=3.exp&libraries=geometry,drawing,places"
-                       loadingElement={<div style={{ height: `100%` }} />}
-                       containerElement={<div style={{ height: `400px` }} />}
-                       mapElement={<div style={{ height: `100%` }} />}
-                       isMarkerShown={true}
-                   />
-
-               </div>
-           );
-       }
-   };
-export default Map;
+import React from 'react'
+import PropTypes from 'prop-types'
+ export default class Map extends React.Component {
+     // Ensure we receive the properties as expected
+    static propTypes = {
+        locations: PropTypes.array.isRequired,
+        searchLocation: PropTypes.func.isRequired
+    }
+     render() {
+         // declaration for easy read and maintain
+        const { locations, searchLocation } = this.props;
+         return (
+             <div id="container">
+                <div>
+                    <input
+                        id="location-input"
+                        className="search-box"
+                        type="text"
+                        placeholder="Search Restaurants"
+                        onChange={(event) => searchLocation(event.target.value)} />
+                </div>
+                 <div className="user-selected-item-container user-selected-item-container-responsive">
+                    {
+                        locations.map((location) => (
+                             <div
+                                key={location.title}
+                                className="user-selected control">
+                            </div>
+                        ))
+                    }
+                </div>
+             </div>
+        )
+    }
+}
