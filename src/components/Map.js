ae50b344e62f7e65188c056ba4d42156fd76ba0f
Th13um <dthieum@gmail.com>
2018-08-28 22:45:55 +0200

    Test for marker on Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import DataLocations from '../data/Locations.json'
 
  class Map extends Component {
 
@@ -9,6 +10,14 @@ import { withGoogleMap, GoogleMap } from 'react-google-maps';
                 defaultCenter={{ lat: 48.864716, lng: 2.349014 }}
                 defaultZoom={12}
             >
+            {DataLocations.map((marker) =>
+                       <Marker
+                          name={marker.title}
+                          position={{ lat: marker.location.lat, lng: marker.location.lng }}
+                          id={marker.id}
+                      >
+                       </Marker>
+                   )}
             </GoogleMap>
         ));
          return (
@@ -17,6 +26,7 @@ import { withGoogleMap, GoogleMap } from 'react-google-maps';
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `500px` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
+                isMarkerShown
                 />
             </div>
         );
