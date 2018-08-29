56a9c8e9a5f0b78f420bc642fb956647b7ec7432
Th13um <dthieum@gmail.com>
2018-08-29 12:47:01 +0200

    Add Accessibility tag and some correction after dev Console audits

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,34 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
- export default class LocationList extends React.Component {
+
+export default class LocationList extends React.Component {
+
      // Ensure we receive the properties as expected
     static propTypes = {
         locations: PropTypes.array.isRequired,
-        searchLocation: PropTypes.func.isRequired
+        searchLocation: PropTypes.func.isRequired,
+        selectLocation: PropTypes.func.isRequired
     }
-     render() {
-         // declaration for easy read and maintain
-        const { locations, searchLocation } = this.props;
-         return (
-             <div id="container">
-                <div>
-                    <input
-                        id="location-input"
-                        type="text"
-                        placeholder="Search Monument"
-                        onChange={(event) => searchLocation(event.target.value)} />
-                </div>
-                 <div className="user-selected-item-container user-selected-item-container-responsive">
-                    {
-                        locations.map((location) => (
-                             <div
-                                key={location.title}
-                                className="user-selected">
-                            </div>
-                        ))
-                    }
-                </div>
-             </div>
-        )
+
+    render() {
+
+      // declaration for easy read and maintain
+      const { locations, searchLocation, selectLocation } = this.props;
+      return (
+        <div id="container" tabIndex="-1">
+          <input
+            autoFocus
+            tabIndex="0"
+            id="location-input"
+            type="text"
+            placeholder="Search Restaurants"
+            aria-label="Search Input Box"
+            role="listitem"
+            onChange={(event) => searchLocation(event.target.value)}/>
+          // For Each Location, display a clickcable marker
+          <div tabIndex="-1"
+            className="user-selected-item-container user-selected-item-container-responsive">
+            { locations.map((location) => (
+              <div
+                onKeyPress={selectLocation.bind(this, location)}
+                aria-label={"Restaurant Name - " + location.title}
+                key={location.title}
+                className="user-selected"
+                tabIndex="0"
+                onClick={selectLocation.bind(this, location)} > {location.title}
+              </div>
+            ))
+            }
+          </div>
+        </div>
+      )
     }
-}
+  }
